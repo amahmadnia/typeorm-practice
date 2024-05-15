46df2e82f86b6acc8bd6e8b615d993c131e9b4bf
Amirabas Ahmadnia <ahmadniaamirabas@gmail.com>
2024-05-16 03:02:27 +0330

    chore: add nodemon script for development

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { AppDataSource } from './data-source';
+
+AppDataSource.initialize().then(() => {
+  const app = express();
+  app.use(express.json());
+  app.get('/', (req, res) => {
+    return res.json('Established connection!');
+  });
+  return app.listen(process.env.PORT, () =>
+    console.log(`Server is running on port ${process.env.PORT}`)
+  );
+});
